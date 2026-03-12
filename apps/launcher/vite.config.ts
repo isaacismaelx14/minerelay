2474f3ff807de6bfc43d85026788d12e0a36f439
Isaac Martinez <isaacismaelx14@gmail.com>
2026-03-12 09:52:11 -0400

    style(launcher): normalize launcher config formatting

--- apps/launcher/vite.config.ts
+++ apps/launcher/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import packageJson from "./package.json";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
